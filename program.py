16a4b7112a1369449deaa8dd2d8ecfb62dd2a58d
Faiz Siddique <faiz@faizs-mbp.lan>
2020-02-10 21:20:32 +0800

    add app

---
+++ program.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+#redis = Redis(host="localhost", port=6379, db=0)
+
+@app.route("/")
+def hello():
+    #visits = redis.incr('counter')
+    html = "<h3>Welcome!</h3>" \
+           #"<b>Visits:</b> {visits}" \
+           #"<br/>"
+    #return html.format(visits=visits)
+    return html
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8082)
