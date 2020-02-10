6204406d792f39785cb2d01cc2343ae551a5723b
Faiz Siddique <faiz@faizs-mbp.lan>
2020-02-11 03:28:46 +0800

    added kube deployments

--- program.py
+++ program.py
@@ -2,7 +2,7 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host="localhost", port=6379, db=0)
+redis = Redis(host="redis", port=6379, db=0)
 
 @app.route("/")
 def hello():
