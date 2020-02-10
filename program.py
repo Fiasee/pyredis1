1a2412252f8d4cee9e7f321aee838f9ebb01bd7c
Faiz Siddique <faiz@faizs-mbp.lan>
2020-02-10 21:25:58 +0800

    some change

--- program.py
+++ program.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     #visits = redis.incr('counter')
-    html = "<h3>Welcome!</h3>" \
+    html = "<h3>Welcome Brother!</h3>" \
            #"<b>Visits:</b> {visits}" \
            #"<br/>"
     #return html.format(visits=visits)
